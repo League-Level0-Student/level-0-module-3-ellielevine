a645568e023ec1c80f57af7809726eab1eea0370
student <student@ip-172-31-61-108.us-west-2.compute.internal>
2021-12-13 17:00:30 -0800

    robot color chooser

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -2,17 +2,58 @@
 package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-	    //2. Make the robot draw a shape (this will take more than one line of code)		
+		Robot robot = new Robot();
+	    //2. Make the robot draw a shape (this will take more than one line of code)	
+		robot.setSpeed(500);
+		robot.penDown();
+		for (int i = 0; i < 3; i++) {
+			robot.move(150);
+			robot.turn(120);
+		}
 		//3. Set the pen width to 10
+		robot.setPenWidth(10);
 		//4. Ask the user what color pen they would like the robot to draw with
+		String color = JOptionPane.showInputDialog("What color pen would you like to draw with?");
 		//5. Use an if/else statement to set the pen color that the user requested
+		if (color.equalsIgnoreCase("red")) {
+			robot.setPenColor(Color.red);
+		}
+		else if (color.equalsIgnoreCase("orange")) {
+			robot.setPenColor(Color.orange);
+		}
+		else if (color.equalsIgnoreCase("yellow")) {
+			robot.setPenColor(Color.yellow);
+		}
+		else if (color.equalsIgnoreCase("green")) {
+			robot.setPenColor(Color.green);
+			}
+		else if (color.equalsIgnoreCase("blue")) {
+			robot.setPenColor(Color.blue);
+		}
+		else if (color.equalsIgnoreCase("pink")) {
+			robot.setPenColor(Color.pink);
+		}
+		robot.setSpeed(500);
+		robot.penDown();
+		for (int i = 0; i < 3; i++) {
+			robot.move(150);
+			robot.turn(120);
+		}
+		}
         //6. If the user doesn't enter anything, choose a random color
+	Random ran = new Random ();
+	rancolor = ran.nextInt
+	else {
+		
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 
@@ -21,4 +62,4 @@ public class RobotColorChooser {
 
 
 	}
-}
+
